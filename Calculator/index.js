dc3143d3ee31fa94ae8e3047ed7581bd24eaaaed
Tushar Rayamajhi <84193854+tusharrayamajhi@users.noreply.github.com>
2023-08-15 21:05:50 +0545

    update

---
+++ Calculator/index.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector('#num');
+let res = document.querySelector('input');
+console.dir(btn);
+
+res.value = btn.innerHTML;
+// console.dir(document.querySelector('input'));
