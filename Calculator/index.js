9520363f61adc9263339856b8b44583655760bfe
Tushar Rayamajhi <tusharrayamajhi956@gmail.com>
2023-09-03 22:56:18 +0545

    add js funcality in code

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,10 +1,26 @@
-let btn = document.querySelectorAll('.num');
-let res = document.querySelector('input');
-function click(){
-    let b = document.querySelector(".num").innerHTML;
-    res.value = b;
-}
-for(bt of btn){
-btn.onclick = click;
-}
+let btn = document.querySelectorAll(".num");
+let res = document.querySelector("input");
+let res2 = document.querySelector("#res2");
+let str = "";
+let a = '';
+res.disabled = true;
+res2.disabled = true;
+btn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+    if(btn.textContent === "="){
+        a = eval(a);
+        res2.value = a;
+    }else if(btn.textContent === "AC")
+    {
+        str = "";
+        a = '';
+        res2.value = "";
+        res.value = "";
+    }else{
+        a += btn.textContent;
+        res.value = a;
+    }
+    });
+});
+
 
