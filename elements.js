88781f37db74fc026f1c8dcd9cc1324a42b7d78b
Tushar Rayamajhi <tusharrayamajhi956@gmail.com>
2023-10-14 07:42:30 +0545

    new update

--- elements.js
+++
@@ -1,8 +0,0 @@
-// import { hellow } from "fun.js";
-let btn = document.createElement("button");
-btn.innerText = "click me";
-document.querySelector("body").appendChild(btn);
-function hello(){
-    console.log("button was clicked");
-}
-btn.onclick = hello;
